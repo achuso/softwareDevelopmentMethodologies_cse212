781847b52125f6d245b282ac8de81f0ef370e41a
achuso <onatribar@protonmail.ch>
2023-05-04 17:30:23 +0300

    added term project assets

---
+++ Term Project/20210702099-SpaceInvaders/src/LoginDatabase.java
@@ -0,0 +1,8 @@
+
+import java.util.Collections;
+
+public class LoginDatabase {
+
+	// import hashtable to manage username-password pairs
+	
+}
---
+++ Term Project/20210702099-SpaceInvaders/src/SpaceObjects.java
@@ -0,0 +1,14 @@
+
+public abstract class SpaceObjects {
+	
+	private int hitPoints;
+	
+	private int HorizontalSpeed;
+	private int VerticalSpeed;
+	
+	private int HorizontalHitboxSize;
+	private int VerticalHitboxSize;
+	
+	
+	
+}
